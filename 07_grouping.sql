6a9fa3b902b167136e44e8556ec5e18198a791b9
skdnjstjr <skdnjstjr0121@naver.com>
2024-03-04 18:01:24 +0900

    no message

---
+++ 07_grouping.sql
@@ -0,0 +1,86 @@
+-- grouping
+-- group by 절은 결과 집합을 특정 열의 값에 따라 그룹화 하는데 사용된다.
+
+select
+	category_code
+from
+	tbl_menu;
+
+select
+	category_code
+from tbl_menu
+group by category_code;
+
+-- count() : 갯수를 세준다.
+select
+	category_code,
+    count(*)
+from
+	tbl_menu
+group by category_code;
+
+-- sum() : 합계
+select
+	category_code,
+    sum(menu_price)
+from
+	tbl_menu
+group by
+	category_code;
+    
+-- avg() : 평균
+select
+	category_code,
+    avg(menu_price)
+from
+	tbl_menu
+group by category_code;
+
+-- 2개 이상의 그룹 생성
+select
+	menu_price,
+    category_code
+from
+	tbl_menu
+group by
+	menu_price,
+    category_code;
+    
+-- with rollup
+-- 컬럼들의 총합을 구하는 방식
+select
+	category_code,
+    sum(menu_price)
+from
+	tbl_menu
+group by
+	category_code
+with rollup;
+
+select
+	menu_price,
+    category_code,
+    sum(menu_price)
+from
+	tbl_menu
+group by
+	menu_price,
+    category_code
+with rollup;
+
+select
+	*
+from
+	tbl_menu;
+
+
+
+    
+
+
+
+
+
+
+
+
